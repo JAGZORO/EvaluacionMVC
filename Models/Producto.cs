6ea68afde99013c800a8c696a689398ed4835232
JAGZORO <145173372+JAGZORO@users.noreply.github.com>
2024-06-04 20:04:07 -0300

    Agregar archivos de proyecto.

---
+++ Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvaluacionMVC.Models;
+
+public partial class Producto
+{
+    public int ProductoId { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string? Descripción { get; set; }
+
+    public decimal Precio { get; set; }
+
+    public int CantidadEnStock { get; set; }
+
+    public DateTime? FechaCreacion { get; set; }
+}
